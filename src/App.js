d457745e1e5881a71337a031a0edb55c21ddbea1
Consgomez <a01026717@itesm.mx>
2022-04-04 21:06:02 -0500

    comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Comp } from "./Components/Comp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Constanza
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Comp />
     </div>
   );
 }
